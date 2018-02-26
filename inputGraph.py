6704c4de0d3319383023a31f89547be58f9ed57e
Spencer Ortega <sportega@csumb.edu>
2018-02-26 03:46:29 +0000

    implemented file streaming and parsing of each line for node1, node2, and weight

--- inputGraph.py
+++ inputGraph.py
@@ -1,8 +1,30 @@
-# file streaming
 
 # store graph in adjacency matrix
     #scipi
     
-file = input("filename: ")
 
+# file streaming
+file = "testData.txt"
 print("yeet: ", file)
+
+f = open(file, "r")
+
+
+if f.mode == 'r':
+    content = f.readlines()
+    print("contents\n",content)
+    i = 0
+    for line in content:
+        data = line.split(" ")
+        n1 = int(data[0])
+        n2 = int(data[1])
+        w = int(data[2])
+        
+        i+=1
+    
+else:
+    exit()
+
+
+
+    
